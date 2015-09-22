424ae35a1aaff6af1698290ecc5046c5e36a5583
Matthew Skone <matt@Matthews-MacBook-Pro.local>
2015-09-22 16:21:01 -0700

    Test for sense().

--- src/tests/robot_test.py
+++ src/tests/robot_test.py
@@ -11,14 +11,15 @@ class RobotTests(unittest.TestCase):
 	"""Unit tests for the Robot class."""
 
 	def setUp(self):
-		robot.mount = MagicMock()
-		robot.sensor = MagicMock()
+		self.mock_sensor = MagicMock()
 		self.r = robot.Robot(driver_module='tests.gopigo_stub')
+		self.r.sensor = self.mock_sensor
 
 	def test_init(self):
 		"""Verify robot's attributes."""
 
 		self.assertEqual(self.r.state, None)
+		self.assertEqual(self.r.sensor, self.mock_sensor)
 
 	def test_run(self):
 		"""Verify run() is delegated to the robot's state."""
@@ -32,3 +33,16 @@ class RobotTests(unittest.TestCase):
 		self.r.state = mock_state
 		self.assertTrue(self.r.run())
 		mock_state.run.assert_called_once_with()
+
+	def test_sense(self):
+		"""Verify sense() is delegated to the sensor."""
+
+		self.mock_sensor.sense.return_value = 50
+		self.assertEqual(self.r.sense(foo='bar'), 50)
+		self.mock_sensor.sense.assert_called_once_with(foo='bar')
+
+	def test_stop(self):
+		"""Verify stop() is delegated to the driver."""
+
+		self.r.stop()
+		self.assertEqual(self.r.driver.calls[-1], 'stop()')
