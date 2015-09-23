0f2154b46a27d4f8da00fcff3c55d64742c76439
Matt Skone <matt.skone@gmail.com>
2015-09-23 10:36:27 -0700

    Flushed out top-level run script.

--- src/run.py
+++ src/run.py
@@ -1,8 +1,30 @@
 """Top-level script for letting the robot run."""
 
+import sys
+import time
+
+import mount
+import robot
+import sensor
+import state
+
 
 def go():
-	pass
+	r = robot.Robot()
+	m = mount.SwivelMount(driver=r.driver, servo_center=93)
+	s = sensor.UltrasonicSensor(driver=r.driver, mount=m)
+	cs = state.CorridorState()
+
+	r.sensor = s
+	r.state = cs
+
+	try:
+		print 'Starting in 5 seconds...'
+		time.sleep(5)
+		r.run()
+	except KeyboardInterrupt:
+		r.stop()
+		sys.exit()
 
 
 if __name__ == '__main__':
