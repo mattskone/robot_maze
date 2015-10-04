b5285e165509740e8483bdd90747f36e57807942
Matt Skone <mskone@zulily.com>
2015-10-04 13:50:48 -0700

    Added utils module

---
+++ src/tests/utils_test.py
@@ -0,0 +1,22 @@
+"""Unit tests for the utils module."""
+
+import unittest
+
+import utils
+
+
+class UtilsTest(unittest.TestCase):
+
+	def test_robot_angle_to_mount_angle(self):
+		test_cases = [
+			(0, 90),
+			(90, 0),
+			(110, 350),
+			(225, 225),
+			(315, 135)
+		]
+
+		for test_case in test_cases:
+			self.assertEqual(
+				utils.robot_angle_to_mount_angle(test_case[0]), test_case[1]
+			)
---
+++ src/utils.py
@@ -0,0 +1,16 @@
+
+def robot_angle_to_mount_angle(robot_angle, mount_zero=90):
+	"""Convert a robot angle to the corresponding mount angle.
+
+	Args:
+	robot_angle - the commanded direction relative to the robot's centerline
+	mount_center - the direction, relative to the robot's centerline, that the
+		mount points when the mount is commanded to angle=0
+
+	Returns the angle, in degrees, to command the mount to point to align to
+		the commanded robot_angle.
+	"""
+
+	# Your code here
+
+	return None
