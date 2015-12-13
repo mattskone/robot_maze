c9b0e54660645921f97078ce26e3d5471979a44c
Matt Skone <matt.skone@gmail.com>
2015-12-13 15:46:53 -0800

    Robot exposes degrees turned

--- src/robot.py
+++ src/robot.py
@@ -13,7 +13,8 @@ TRIM_STRAIGHT = -10		# Trim setting for straight movement
 DEFAULT_SPEED = 60  	# Slowest speed without stall (60/120 for batt/cable)
 TURN_SPEED = 10			# Added speed for the outside wheel when turning
 MAX_TURN_RATIO = 1.2	# Max ratio of outside wheel to inside wheel speeds
-DEGREES_PER_TICK = 10	# Degrees of robot rotation in one encoder tick
+ROTATING_DEGREES_PER_TICK = 10	# Degrees of robot rotation in one encoder tick
+TURNING_DEGREES_PER_TICK = 5	# Degrees of robot turn in one encoder tick
 
 
 class Robot(object):
@@ -46,6 +47,8 @@ class Robot(object):
 		self.speed = [0, 0]  # [left, right motor]
 		self.driver.set_speed(DEFAULT_SPEED)
 		self.driver.trim_write(TRIM_STRAIGHT)
+		self.left_encoder = 0
+		self.right_encoder = 0
 
 	def run(self):
 		"""Set the robot in motion.
@@ -59,6 +62,25 @@ class Robot(object):
 			# TODO: make the robot determine its state before proceding
 			raise AttributeError('State attribute not set on Robot.')
 		return self.state.run()
+
+	@property
+	def degrees_turned(self):
+		"""Return the net degrees of turn since last accessed.
+
+		Positive values indicate a net right turn, and negative values
+		indicate a net left turn.
+		"""
+
+		left_encoder = self.driver.enc_read(MOTOR_LEFT)
+		right_encoder = self.driver.enc_read(MOTOR_RIGHT)
+		diff_left = left_encoder - self.left_encoder
+		diff_right = right_encoder - self.right_encoder
+		degrees = (diff_left - diff_right) * TURNING_DEGREES_PER_TICK
+
+		self.left_encoder = left_encoder
+		self.right_encoder = right_encoder
+
+		return degrees
 
 	def dist(self, angle=0):
 		"""Take an return a distance sensor reading in the direction given."""
@@ -88,10 +110,10 @@ class Robot(object):
 
 		self.driver.stop()
 		if degrees > 0:
-			self.driver.enc_tgt(1, 0, int(degrees / DEGREES_PER_TICK))
+			self.driver.enc_tgt(1, 0, int(degrees / ROTATING_DEGREES_PER_TICK))
 			self.driver.right_rot()
 		else:
-			self.driver.enc_tgt(0, 1, abs(int(degrees / DEGREES_PER_TICK)))
+			self.driver.enc_tgt(0, 1, abs(int(degrees / ROTATING_DEGREES_PER_TICK)))
 			self.driver.left_rot()
 
 	def steer(self, steering_factor):
@@ -114,6 +136,5 @@ class Robot(object):
 			self.speed = [turn_wheel_speed, DEFAULT_SPEED]
 		else:
 			self.speed = [DEFAULT_SPEED, DEFAULT_SPEED]
-		print self.speed
 		self.driver.set_left_speed(self.speed[0])
 		self.driver.set_right_speed(self.speed[1])
